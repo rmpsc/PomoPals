cf9181aea925e374f7c1c7ec17f00074ab82f364
Romel Pascua <romelpascua14@gmail.com>
2023-08-12 22:18:36 -0700

    clean imports

--- pages/PomodoroGroup.tsx
+++ pages/PomodoroGroup.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Stack, Text, Button, Theme } from 'tamagui';
+import { Stack, Text, Button } from 'tamagui';
 /* https://tamagui.dev/docs/core/stack-and-text  */
 
 interface PomodoroGroupProps {}
