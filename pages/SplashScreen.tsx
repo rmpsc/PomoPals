e35c46fb40d3a075b28a434cfe4da467930ecd6b
Romel Pascua <romelpascua14@gmail.com>
2023-10-11 18:20:56 -0700

    change hidden activity indicator

--- pages/SplashScreen.tsx
+++ pages/SplashScreen.tsx
@@ -9,7 +9,7 @@ function SplashScreen() {
         source={require('../assets/tomapal.png')}
         style={styles.image}
       />
-      <ActivityIndicator color={'white'} />
+      <ActivityIndicator color={'black'} />
     </YStack>
   );
 }
